853c0ee46c704c47d4de1f813b65774db6500f16
HJS-HJS <85570804+HJS-HJS@users.noreply.github.com>
2024-04-17 13:47:13 +0900

    Add files via upload

---
+++ apply_dot_depth.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+from utils import *
+from ir_pattern import IRPattern
+
+intrinsic = np.array([[893.82104492   ,0.             ,633.12652588],
+                      [0.0            ,893.82104492   ,354.45303345],
+                      [0.0            ,0.             ,1.          ]])
+
+extrinsic = np.array([ -9.5284142039327635e-01, 2.1412216810855048e-02, 3.0271231318558661e-01, -1.0649844006838340e+00,
+        1.2093728749856781e-02, 9.9939450765343596e-01, -3.2624527537913145e-02, 9.5766369044197786e-02,
+        -3.0322758665373545e-01, -2.7425080553976539e-02, -9.5252343574778220e-01, 9.5766369044197786e-01,
+        0., 0., 0., 1. ]).reshape(4,4)
+
+# Distance difference between two cameras.
+cam_dist = 0.055
+
+# Get depth, gray image.
+gray0  = cv2.imread("gray_image_0.png", cv2.IMREAD_GRAYSCALE)
+gray1  = cv2.imread("gray_image_1.png", cv2.IMREAD_GRAYSCALE)
+depth0 = np.load('depth_image_0.npy')
+depth1 = np.load('depth_image_1.npy')
+
+# Initialize IRPattern module.
+ir_pattern = IRPattern(depth0.shape, intrinsic)
+
+# Get IR pattern map from each depth image.
+depth_dot0 = ir_pattern.ir_matrix_from_depth(depth0, cam_dist / 2)
+depth_dot1 = ir_pattern.ir_matrix_from_depth(depth1, -cam_dist / 2)
+
+# Apply IR pattern to gray image.
+gray0[np.where(depth_dot0)] = (gray0[np.where(depth_dot0)] + 1) * 1.5
+gray1[np.where(depth_dot1)] = (gray1[np.where(depth_dot1)] + 1) * 1.5
+
+# Create depth image from two gray image.
+depth = img_to_depth(gray0, gray1, intrinsic)
+
+# Visualize.
+fig = plt.figure()
+ax = fig.add_subplot(221)
+ax.imshow(gray0, cmap='gray')
+ax = fig.add_subplot(222)
+ax.imshow(gray1, cmap='gray')
+ax = fig.add_subplot(223)
+ax.imshow(depth, cmap='gray')
+ax = fig.add_subplot(224)
+ax.imshow(ir_pattern.gen_simple_ir_pattern(), cmap='gray')
+plt.show()
---
+++ ir_pattern.py
@@ -0,0 +1,151 @@
+import copy
+from typing import Tuple, List
+import numpy as np
+
+class IRPattern(object):
+    def __init__(self, image_shape: Tuple[int, int], camera_intrinsic: np.array):
+        """Create an xy coordinate matrix of the camera image.
+
+        Args:
+            image_shape (Tuple[int, int]): (2) shape of image to add ir patterns
+            camera_intrinsic (np.array): (3, 3) camera intrinsic matrix.
+        """
+        # Save the shape of the image.
+        cols = np.arange(image_shape[1])
+        rows = np.arange(image_shape[0])
+        self.image_shape = image_shape
+
+        # Create a coordinate matrix with the pixel positions of the image as x and y values.
+        self.coords = np.empty((len(rows), len(cols), 2), dtype=np.float32)
+        self.coords[:,:,0] = rows[:, None]
+        self.coords[:,:,1] = cols
+
+        # Save the camera intrinsic data.
+        self.fx = camera_intrinsic[0][0]
+        self.fy = camera_intrinsic[1][1]
+        self.cx = camera_intrinsic[0][2]
+        self.cy = camera_intrinsic[1][2]
+
+    def gen_simple_ir_pattern(self, transpose: float=0, dot_length: float=12.6, dot_diameter: float=3, rotation: List[float]=[50, 60])-> np.array:        
+        # Generate IR pattern matrix from example depth image with a depth of 1.
+        return self.ir_matrix_from_depth(np.ones(self.image_shape), transpose, dot_length, dot_diameter, rotation)
+
+    def ir_matrix_from_depth(self, depth_image: np.array, transpose: float, dot_length: float=12.6, dot_diameter: float=3, rotation: List[float]=[50, 60])-> np.array:
+        """A function that creates an IR pattern matrix using the values ​​of the depth image. A matrix of the same size as the Input Depth image is created, recording whether an IR pattern exists at the corresponding pixel location.
+
+        Args:
+            depth_image (np.array): (H, W) depth image to check IR pattern.
+            transpose (float): A value for how far the camera is from the IR pattern projector in hardware. The IR projector and camera are always spaced apart in the width direction.
+            dot_length (float, optional): Dot spacing in IR pattern. Defaults to 12.6.
+            dot_size (float, optional): Dot size in IR pattern. Defaults to 3.
+            rotation (List[float], optional): List of tilt angles for IR patterns. Defaults to [50, 60].
+
+        Returns:
+            np.array: (H, W) boolean type matrix. Pixels with an IR pattern are True.
+        """
+        # Check whether shape of depth image and image from initial function is same.
+        if depth_image.shape != self.coords.shape[:2]:
+            raise Exception("The shape of depth image and image in initial are mismatched")
+        
+        # The diameter of the dot when the dot length becomes unit length.
+        _unit_dot_size = dot_diameter / dot_length
+        # A matrix that sets the pixels of the depth image with the it pattern to 'True'. Output of this function.
+        _matrix = np.zeros(depth_image.shape, dtype=bool)
+        # Copy xy coordinate matrix from self.coords in init function.
+        _img = copy.deepcopy(self.coords)
+
+        # Convert each depth points in image into camera coordinate.
+        _img[:,:,0] = (_img[:,:,0] - self.cy) * np.abs(depth_image) / self.fy
+        _img[:,:,1] = (_img[:,:,1] - self.cx) * np.abs(depth_image) / self.fx
+
+        # Transpose each depth points to IR projector coordinate.
+        _img[:,:,1] -= transpose
+
+        # Distance from each points and IR projector.
+        _img_radius = np.linalg.norm(np.concatenate((_img,depth_image.reshape(self.image_shape[0], self.image_shape[1], 1)),axis=2), axis=2)
+
+        # Pattern calculation when each point is at a distance of 1 from the IR projector.
+        _img[:,:,0] /= _img_radius
+        _img[:,:,1] /= _img_radius
+
+        # Convert points to IR projector image coordinate.
+        _img[:,:,0] = _img[:,:,0] * self.fy + self.cy
+        _img[:,:,1] = _img[:,:,1] * self.fx + self.cx
+
+        # Calculates patterns according to all angles.
+        for _rad in np.deg2rad(rotation):
+            # Rotation matrix
+            _R = np.array([[np.cos(_rad), np.sin(_rad)], [-np.sin(_rad),  np.cos(_rad)]])
+
+            # For convenience, the coordinates of the points are divided by the unit length and all points are rotated.
+            _rot_img = (_img[:,:,]/ dot_length).dot(_R)
+            
+            # Move each point to find the distance to the nearest vertex.
+            _rot_img[:,:,] -= np.round(_rot_img[:,:,])
+                
+            # Calculate distance from origin
+            _rot_img = np.linalg.norm(_rot_img, axis=2)
+    
+            # True if smaller than IR pattern dot size
+            _matrix[np.where(_rot_img < _unit_dot_size)] = True
+
+        # Output is a matrix in which pixels with IR patterns are True.
+        return _matrix
+    def ir_matrix_from_depth_plane(self, depth_image: np.array, transpose: float, dot_length: float=12.6, dot_diameter: float=3, rotation: List[float]=[50, 60])-> np.array:
+        """A function that creates an IR pattern matrix using the values ​​of the depth image. A matrix of the same size as the Input Depth image is created, recording whether an IR pattern exists at the corresponding pixel location.
+        Unlike function ir_matrix_from_depth, an IR pattern without distortion is generated. 
+
+        Args:
+            depth_image (np.array): (H, W) depth image to check IR pattern.
+            transpose (float): A value for how far the camera is from the IR pattern projector in hardware. The IR projector and camera are always spaced apart in the width direction.
+            dot_length (float, optional): Dot spacing in IR pattern. Defaults to 12.6.
+            dot_size (float, optional): Dot size in IR pattern. Defaults to 3.
+            rotation (List[float], optional): List of tilt angles for IR patterns. Defaults to [50, 60].
+
+        Returns:
+            np.array: (H, W) boolean type matrix. Pixels with an ir pattern are True.
+        """
+        # Check whether shape of depth image and image from initial function is same.
+        if depth_image.shape != self.coords.shape[:2]:
+            raise Exception("The shape of depth image and image in initial are mismatched")
+        
+        # The diameter of the dot when the dot length becomes unit length.
+        _unit_dot_size = dot_diameter / dot_length
+        # A matrix that sets the pixels of the depth image with the it pattern to 'True'. Output of this function.
+        _matrix = np.zeros(depth_image.shape, dtype=bool)
+        # Copy xy coordinate matrix from self.coords in init function.
+        _img = copy.deepcopy(self.coords)
+
+        # Convert each depth points in image into camera coordinate.
+        _img[:,:,0] = (_img[:,:,0] - self.cy) * np.abs(depth_image) / self.fy
+        _img[:,:,1] = (_img[:,:,1] - self.cx) * np.abs(depth_image) / self.fx
+        
+        # Transpose each depth points to IR projector coordinate.
+        _img[:,:,1] -= transpose
+        # Move all points to a position with depth 1.
+        _img[:,:,0] /= np.abs(depth_image)
+        _img[:,:,1] /= np.abs(depth_image)
+
+        # Convert points to IR projector image coordinate.
+        _img[:,:,0] = _img[:,:,0] * self.fy + self.cy
+        _img[:,:,1] = _img[:,:,1] * self.fx + self.cx
+
+        # Calculates patterns according to all angles.
+        for _rad in np.deg2rad(rotation):
+            # Rotation matrix
+            _R = np.array([[np.cos(_rad), np.sin(_rad)], [-np.sin(_rad),  np.cos(_rad)]])
+
+            # For convenience, the coordinates of the points are divided by the unit length and all points are rotated.
+            _rot_img = (_img[:,:,]/ dot_length).dot(_R)
+            
+            # Move each point to find the distance to the nearest vertex.
+            _rot_img[:,:,] -= np.round(_rot_img[:,:,])
+                
+            # Calculate distance from origin
+            _rot_img = np.linalg.norm(_rot_img, axis=2)
+    
+            # True if smaller than IR pattern dot size
+            _matrix[np.where(_rot_img < _unit_dot_size)] = True
+
+        # Output is a matrix in which pixels with IR patterns are True.
+        return _matrix
